5f66d5a340dcbfc2338410b8fdac35aebb281f3f
LianaLing <lianalingliya@gmail.com>
2021-08-31 17:28:37 +0800

    Added algos to main prog, not yet run

--- summarize/__main__.py
+++ summarize/__main__.py
@@ -1,25 +1,35 @@
 import sys
 import re
 import argparse
-from summarize.ai import naive
+from summarize.ai import naive, Bart, Pegasus, T5
 from summarize.scraper import parser
 from typing import List
 
 
 def _is_valid_url(url: str) -> bool:
     regex = re.compile(
-        r'^(?:http|ftp)s?://'  # http:// or https://
+        r"^(?:http|ftp)s?://"  # http:// or https://
         # domain...
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
-        r'localhost|'  # localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-        r'(?::\d+)?'  # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+        r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"
+        r"localhost|"  # localhost...
+        r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or ip
+        r"(?::\d+)?"  # optional port
+        r"(?:/?|[/?]\S+)$",
+        re.IGNORECASE,
+    )
 
     return re.match(regex, url) is not None
 
 
-def summarize_url(url: str, tags: List[str] = ['p'], method: str = 'naive'):
+def set_max_length(length: str) -> int:
+    max_dict = {"short": 200, "medium": 600, "long": 1000}
+
+    return max_dict.get("length")
+
+
+def summarize_url(
+    url: str, tags: List[str] = ["p"], method: str = "naive", length: str = "short"
+):
     # Get the content from the url
     content = parser.parse_html_to_paragraphs(url, tags)
 
@@ -28,11 +38,24 @@ def summarize_url(url: str, tags: List[str] = ['p'], method: str = 'naive'):
         print(f"No content of {tags} is fetched from {url}")
         sys.exit(0)
 
+    # Set summary length
+    min_length = 1
+    max_length = set_max_length(length)
+
     summary = ""
 
     # Summarize the content
     if method == "naive":
         summary = naive.summarize(content)
+    elif method == "bart":
+        bart = Bart()
+        summary = bart.summarize(content, min_length, max_length)
+    elif method == "pegasus":
+        pegasus = Pegasus()
+        summary = pegasus.summarize(content, min_length, max_length)
+    elif method == "t5":
+        t5 = T5()
+        summary = t5.summarize(content, min_length, max_length)
 
     # Print the summary to stdout
     print(summary)
@@ -44,11 +67,29 @@ def summarize_url(url: str, tags: List[str] = ['p'], method: str = 'naive'):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("url", help="the website's url", type=str)
-    parser.add_argument("-t",
-                        "--tags", help="tags that is read from the website, eg. --tag p h1 h2", default=["p"], nargs="*")
-    parser.add_argument("-m",
-                        "--method", help="method used for summarizing (naive, textrank, seq2seq)", default="naive", choices=["naive", "textrank", "seq2seq"])
+    parser.add_argument(
+        "-t",
+        "--tags",
+        help="tags that is read from the website, eg. --tag p h1 h2",
+        default=["p"],
+        nargs="*",
+    )
+    parser.add_argument(
+        "-m",
+        "--method",
+        help="method used for summarizing (naive, textrank, seq2seq)",
+        default="naive",
+        choices=["naive", "textrank", "seq2seq", "bart", "t5", "pegasus"],
+    )
     # Add an argument for specifying 'short, medium, long'
+    parser.add_argument(
+        "-l",
+        "--length",
+        help="the length of the summary",
+        type=str,
+        default="short",
+        choices=["short", "medium", "long"],
+    )
 
     # Parse the arguments
     args = parser.parse_args()
@@ -58,7 +99,7 @@ def main():
         parser.error("url must be a valid url eg. https://www.google.com")
 
     # Summarize the website url
-    summarize_url(args.url, args.tags, args.method)
+    summarize_url(args.url, args.tags, args.method, args.length)
 
 
 if __name__ == "__main__":
--- summarize/scraper/parser.py
+++ summarize/scraper/parser.py
@@ -12,15 +12,42 @@ def parse_html_to_paragraphs(url: str, tags: List[str]) -> str:
     html_content = _get_html_text(url)
 
     # Parsing the URL content and storing in a variable
-    parsed = BeautifulSoup(html_content, 'html.parser')
+    parsed = BeautifulSoup(html_content, "html.parser")
 
     # Get all the content in the tas
     paragraphs = parsed.find_all(tags)
 
-    content = ''
+    content = ""
 
     # Looping through the paragraphs and adding them to the variable
     for paragraph in paragraphs:
         content += paragraph.text
 
     return content
+
+
+# The content variable should be parsed into paragraphs
+# Used by BART
+def chunk_text(content: str) -> List[str]:
+    max_chunk = 500
+    current_chunk = 0
+    chunks = []
+
+    for sentence in content:
+        if len(chunks) == current_chunk + 1:
+            # Check if the chunk is less than 500 words
+            if len(chunks[current_chunk]) + len(sentence.split(" ")) <= max_chunk:
+                chunks[current_chunk].extend(sentence.split(" "))
+            # Next chunk
+            else:
+                current_chunk += 1
+                chunks.append(sentence.split(" "))
+        else:
+            print(current_chunk)
+            chunks.append(sentence.split(" "))
+
+    # Append words to chunks so that each chunk is less than 500 words
+    for chunk_id in range(len(chunks)):
+        chunks[chunk_id] = " ".join(chunks[chunk_id])
+
+    return chunks
